e20d2b45c1b73359a90ebda9550a19263fe3b3cd
Kirill <freshjs.ftd@gmail.com>
2021-09-07 16:00:41 +0300

    Add task

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,11 @@
-const btn = document.querySelector('#btn');
+/*
+Создать функции которые последовательно выводят в консоль числа от 1 до 20 с интервалом в 100 мс.
+Можно решить через 
+- setInterval - if, clearInterval
+- setTimeout - рекурсия с if
 
-btn.addEventListener('click', (e) => {});
+Посчисать сколько времени это все дело заняло
+console.time() 
+console.timeEnd()
 
-console.log('Hi!');
-
-const id = setTimeout(function callback1() {
-  console.log('Hello World');
-
-  // setTimeout(function callbackDeep () {
-  //   console.log(1234);
-  // }, 1000)
-}, 0);
-
-clearTimeout(id);
-
-let i = 0;
-// const intervalId = setInterval(()=>{console.log(`interval${i++}`)}, 100);
-
-// clearInterval(intervalId);
-
-console.log('END.');
+*/
