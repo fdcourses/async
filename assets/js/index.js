466633d53d693d1871285173490f3feb5021ea34
Kirill <freshjs.ftd@gmail.com>
2021-09-07 17:12:18 +0300

    Add json

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
 /*
-Создать функции которые последовательно выводят в консоль числа от 1 до 20 с интервалом в 100 мс.
+Создать функции которые последовательно выводят в консоль числа от 1 до 20 
+с интервалом в 100 мс.
 Можно решить через 
 - setInterval - if, clearInterval
 - setTimeout - рекурсия с if
@@ -7,5 +8,40 @@
 Посчисать сколько времени это все дело заняло
 console.time() 
 console.timeEnd()
-
 */
+
+// function countInterval() {
+//   let counter = 1;
+  
+//   const intervalId = setInterval(()=>{
+//     console.log(counter++);
+
+//     if(counter > 20) {
+//       clearInterval(intervalId);
+//       console.timeEnd('interval');
+//     }
+//   },100);
+// }
+
+// console.time('interval'); 
+// countInterval();
+
+
+const obj = {
+  id: 1,
+  name: 'test',
+  lastName: 'test',
+  age: 100,
+  location: undefined,
+  test() {
+
+  },
+  [Symbol()]: 'test'
+
+};
+
+const arr = [1,2,3]
+
+const jsonifiedObj = JSON.stringify(arr); // сериализация чегото
+
+const rebuildedObj = JSON.parse(jsonifiedObj); // десериализция
