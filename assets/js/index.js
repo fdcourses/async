b154dcad037cb2dd48a083d2c6c7fc9b1a3f23af
Kirill <freshjs.ftd@gmail.com>
2021-09-08 17:02:22 +0300

    Add Promise constructor

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,10 +2,41 @@
 
 // ====================================================================
 fetch('./assets/js/dadfvbdfta.json')
-  .then((response) => response.json(),(err)=>{console.log('обработка ошибок в then')})
+  .then(
+    (response) => response.json(),
+    (err) => {
+      console.log('обработка ошибок в then');
+    }
+  )
   .then((data) => {
     console.log(data);
   })
   .catch((err) => {
     console.log('обработка ошибок в catch');
   });
+
+function exec(resolve, reject) {
+  resolve();
+  reject();
+}
+
+const promise = new Promise(exec);
+
+const ratPromise = new Promise((resolve, reject) => {
+  const verdict = Math.random();
+
+  if (verdict > 0.5) {
+    resolve('крыса жива');
+  } else {
+    reject('крыса почти жива');
+  }
+});
+
+ratPromise.then(
+  (data) => {
+    console.log(data);
+  },
+  (errorData) => {
+    console.log(errorData);
+  }
+);
