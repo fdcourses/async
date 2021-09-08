2350321e5aebca368569c1bd3b7c5c4e2104f760
Kirill <freshjs.ftd@gmail.com>
2021-09-08 16:20:58 +0300

    Add fetch example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,47 +1,14 @@
-/*
-Создать функции которые последовательно выводят в консоль числа от 1 до 20 
-с интервалом в 100 мс.
-Можно решить через 
-- setInterval - if, clearInterval
-- setTimeout - рекурсия с if
+'use strict';
 
-Посчисать сколько времени это все дело заняло
-console.time() 
-console.timeEnd()
-*/
+// ====================================================================
+fetch('./assets/js/data.json')
+  .then((response) => {
+    console.log('завершился запрос fetch');
+    return response.json(); // пытаемся превратить кашу в что-то читаемое
+  })
+  .then((data) => {
+    console.log('превратили данные в джаваскриптовый массив');
+    console.log(data);
+  });
 
-// function countInterval() {
-//   let counter = 1;
-  
-//   const intervalId = setInterval(()=>{
-//     console.log(counter++);
-
-//     if(counter > 20) {
-//       clearInterval(intervalId);
-//       console.timeEnd('interval');
-//     }
-//   },100);
-// }
-
-// console.time('interval'); 
-// countInterval();
-
-
-const obj = {
-  id: 1,
-  name: 'test',
-  lastName: 'test',
-  age: 100,
-  location: undefined,
-  test() {
-
-  },
-  [Symbol()]: 'test'
-
-};
-
-const arr = [1,2,3]
-
-const jsonifiedObj = JSON.stringify(arr); // сериализация чегото
-
-const rebuildedObj = JSON.parse(jsonifiedObj); // десериализция
+console.log(fetchResult);
